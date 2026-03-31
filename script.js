0db43e60364fbc12b266ff04efcc33c2fea7518b
ajeem-khan <ajeem0987654@gmail.com>
2026-03-31 12:01:49 +0530

    Update alert message for code copy confirmation

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function copyCode(id) {
     let code = document.getElementById(id).innerText;
     navigator.clipboard.writeText(code);
-    alert("Code copied 😎");
+
+    alert("✅ Code copied!");
 }
 
 function searchCode() {
